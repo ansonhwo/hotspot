3ea40d1e3d499aab065788d2f318d7c7def8a618
Anson Wong <ansonwong@Ansons-Mac.local>
2017-01-10 22:38:47 -0800

    Added non-functional search bar and search button. #1

--- public/main.js
+++ public/main.js
@@ -1,50 +1,8 @@
-const app = new Vue({
-  el: '#app',
-  data: {
-    message: 'Hello world!'
-  }
-})
-
-const app2 = new Vue({
-  el: "#app2",
-  data: {
-    message: 'You loaded this page on ' + new Date()
-  }
-})
-
-const app3 = new Vue({
-  el: '#app3',
-  data: {
-    seen: true
-  }
-})
-
-const app4 = new Vue({
-  el: '#app4',
-  data: {
-    todos: [
-      { text: 'Learn JavaScript' },
-      { text: 'Learn Vue' },
-      { text: 'Build something awesome' }
-    ]
-  }
-})
-
-const app5 = new Vue({
-  el: '#app5',
-  data: {
-    message: 'Hello Vue.js'
-  },
+const searchBar = new Vue({
+  el: '#search',
   methods: {
-    reverseMessage: function() {
-      this.message = this.message.split('').reverse().join('')
+    searchEvents: function() {
+      console.log('searching events')
     }
-  }
-})
-
-const app6 = new Vue({
-  el: '#app6',
-  data: {
-    message: 'Hello world!'
   }
 })
